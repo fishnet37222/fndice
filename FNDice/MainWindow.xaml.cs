e4eb308b7731390eb4b0e7201edf127b6395dc23
David Frischknecht <fishnet37222@gmail.com>
2024-11-07 20:06:15 -0500

    Initial commit.

---
+++ FNDice/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2024 David A. Frischknecht
+//
+// SPDX-License-Identifier: Apache-2.0
+
+namespace FNDice;
+
+public partial class MainWindow
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
