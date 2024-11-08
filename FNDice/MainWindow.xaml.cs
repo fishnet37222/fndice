c0e4c0e2bcd7e332f4548700362a30d8068786ef
David Frischknecht <fishnet37222@gmail.com>
2024-11-07 22:42:13 -0500

    MainWindow:  Layout the UI.

--- FNDice/MainWindow.xaml.cs
+++ FNDice/MainWindow.xaml.cs
@@ -2,12 +2,30 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Windows;
+
 namespace FNDice;
 
 public partial class MainWindow
 {
+	private Die[] m_dice = new Die[5];
 	public MainWindow()
 	{
 		InitializeComponent();
+	}
+
+	private void BtnRollDice_OnClick(object sender, RoutedEventArgs e)
+	{
+		foreach (var die in m_dice)
+		{
+			if (die.IsSelected) continue;
+			die.Roll();
+		}
+	}
+
+	private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+	{
+		var dice = m_grdDice.Children.OfType<Die>().ToArray();
+		m_dice = dice;
 	}
 }
