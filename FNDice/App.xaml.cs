b742368a64943d613efcb16225ce826bcef5ed51
David Frischknecht <fishnet37222@gmail.com>
2025-03-03 18:48:17 -0500

    Initial commit.

---
+++ FNDice/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2025 David A. Frischknecht
+//
+// SPDX-License-Identifier: Apache-2.0
+
+namespace FNDice;
+
+public partial class App
+{
+}
---
+++ FNDice/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2025 David A. Frischknecht
+//
+// SPDX-License-Identifier: Apache-2.0
+
+namespace FNDice;
+
+public partial class MainWindow
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
