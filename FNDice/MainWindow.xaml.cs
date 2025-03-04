26d6bce6db2241d276e7c28e4499e500695ea429
David Frischknecht <fishnet37222@gmail.com>
2025-03-04 03:06:42 -0500

    MainWindow:  Add the dice and the button to roll them.

--- FNDice/MainWindow.xaml.cs
+++ FNDice/MainWindow.xaml.cs
@@ -2,12 +2,25 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Windows;
+
 namespace FNDice;
 
 public partial class MainWindow
 {
+	private readonly Die[] m_dice;
+
 	public MainWindow()
 	{
 		InitializeComponent();
+		m_dice = [m_die1, m_die2, m_die3, m_die4, m_die5];
+	}
+
+	private void BtnRoll_OnClick(object sender, RoutedEventArgs e)
+	{
+		foreach (var die in m_dice)
+		{
+			if (!die.IsSelected) die.Roll();
+		}
 	}
 }
